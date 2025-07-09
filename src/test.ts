9eb670bc9feb5de6054aff90e28e9cd17e30bdeb
JasserGR <jasser.gorsia@enicar.ucar.tn>
2025-07-09 13:32:44 +0100

    fixing testing issues

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,22 @@
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+declare var require: {
+  context(path: string, deep?: boolean, filter?: RegExp): {
+    keys(): string[];
+    <T>(id: string): T;
+  };
+};
 
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-  { teardown: { destroyAfterEach: true } }
+  platformBrowserDynamicTesting()
 );
 
 const context = require.context('./', true, /\.spec\.ts$/);
-context.keys().map(context);
+context.keys().forEach(context);
+
