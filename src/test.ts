4253e51fb32dbf4a110d3bf2bf6f1a95c3945887
JasserGR <jasser.gorsia@enicar.ucar.tn>
2025-07-09 14:04:58 +0100

    fixing test

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,17 @@
-import 'zone.js';
+import 'zone.js'; 
 import 'zone.js/testing';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare let require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
-
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
 
-const allSpecFiles = import.meta.glob('./**/*.spec.ts', { eager: true });
 
+import.meta.glob('./**/*.spec.ts', { eager: true });
 
