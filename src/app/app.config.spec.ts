e9cd58f2973d269fefe3312ef3a053029dc94a16
JasserGR <jasser.gorsia@enicar.ucar.tn>
2025-07-12 17:25:14 +0100

    Fix TypeScript errors in app.config.spec.ts by removing invalid ApplicationConfig instantiation

--- src/app/app.config.spec.ts
+++ src/app/app.config.spec.ts
@@ -1,16 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appConfig } from './app.config';
 import { routes } from './app.routes';
 
 describe('appConfig', () => {
   it('should create an ApplicationConfig with correct providers', () => {
-    const config = new ApplicationConfig(); 
     expect(appConfig).toBeDefined();
     expect(appConfig.providers).toBeDefined();
     expect(appConfig.providers.length).toBe(2);
     expect(appConfig.providers[0]).toEqual(provideZoneChangeDetection({ eventCoalescing: true }));
     expect(appConfig.providers[1]).toEqual(provideRouter(routes));
-    expect(config).toBeInstanceOf(ApplicationConfig); 
+
   });
 });
